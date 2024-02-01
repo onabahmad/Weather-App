1a3805ba47bbc2930b3b474c8318ce8bba2f2c18
Onab <onab45@gmail.com>
2024-02-01 20:04:13 +0530

    fixed forecast date issue

--- src/services/WeatherProvider.js
+++ src/services/WeatherProvider.js
@@ -1,4 +1,3 @@
-// Import necessary dependencies and components
 import { DateTime } from "luxon";
 
 // API key and base URL for OpenWeatherMap API
@@ -42,33 +41,25 @@ const formatCurrentWeather = (data) => {
   };
 };
 
-// Function to format timestamp to local time
 const formatToLocalTime = (
   secs,
   zone,
   format = "cccc, dd LLL yyyy'| Local time: 'hh:mm a"
 ) => DateTime.fromSeconds(secs).setZone(zone).toLocaleString({ format });
 
-// Function to format forecast data with average temperature
 const formatForecastDataWithAverage = (data) => {
-  // Map to store daily temperatures, counts, descriptions, and icons
   const dailyData = new Map();
 
-  // Iterate over forecast data
   data.list.forEach((forecast) => {
-    const date = formatToLocalTime(
-      forecast.dt,
-      data.city.timezone,
-      "dd LLL yyyy"
-    );
+    const date = DateTime.fromFormat(
+      forecast.dt_txt,
+      "yyyy-MM-dd HH:mm:ss"
+    ).toFormat("yyyy-MM-dd");
 
-    // Check if the date is already in the map
     if (dailyData.has(date)) {
-      // Update the sum, count, description, and icon
       dailyData.get(date).sum += forecast.main.temp;
       dailyData.get(date).count += 1;
     } else {
-      // Add a new entry to the map with description and icon
       dailyData.set(date, {
         sum: forecast.main.temp,
         count: 1,
@@ -78,7 +69,6 @@ const formatForecastDataWithAverage = (data) => {
     }
   });
 
-  // Calculate average temperature for each day and use the first entry's description and icon
   const formattedData = Array.from(dailyData.entries()).map(
     ([date, { sum, count, description, icon }]) => {
       return {
@@ -93,20 +83,17 @@ const formatForecastDataWithAverage = (data) => {
   return formattedData;
 };
 
-// Update the main export to use the new function
 const getFormattedWeatherData = async (searchParams) => {
-  // Fetch current weather data
   const currentWeatherData = await getWeatherData("weather", searchParams).then(
     formatCurrentWeather
   );
 
-  // Fetch 5-day forecast data
   const forecastData = await getWeatherData("forecast", searchParams).then(
     formatForecastDataWithAverage
   );
+  const trimmedForecastData = forecastData.slice(1);
 
-  // Return an object with formatted current and forecast weather data
-  return { currentWeatherData, forecastData };
+  return { currentWeatherData, forecastData: trimmedForecastData };
 };
 
 const iconURLFromCode = (code) =>
